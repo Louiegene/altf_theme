1d6b1e461512204f55a1f20a53506fe5d779e103
ltdonato <donato.louiegene@gmail.com>
2026-04-14 22:50:39 +0800

    feat: modernize UI theme with custom typography, refined card styling, responsive tables, and updated navigation icons.

--- vertical-responsive-menu.js
+++ vertical-responsive-menu.js
@@ -1,47 +1,26 @@
-/*
-==========================
-Vertical Responsive Menu
-==========================
-*/
-
 'use strict';
-
 
 var tid = setInterval( function () {
   if ( document.readyState !== 'complete' ) return;
   clearInterval( tid );
 
-
-  var querySelector = document.querySelector.bind(document);
-
-  var nav = document.querySelector('.vertical_nav');
-  var wrapper = document.querySelector('.wrapper');
-
-  var menu = document.getElementById("js-menu");
-  var subnavs = menu.querySelectorAll('.menu--item__has_sub_menu');    
-
-  // Open Sub Menu
+  var subnavs = document.querySelectorAll('.menu--item__has_sub_menu');    
   
   for (var i = 0; i < subnavs.length; i++) {
-    
-    if (subnavs[i].classList.contains('menu--item__has_sub_menu') ) {
-      
-      subnavs[i].querySelector('.menu--link').addEventListener('click', function (e) {
-
+    var link = subnavs[i].querySelector('.menu--link');
+    if(link) {
+      link.addEventListener('click', function (e) {
+          var currentItem = e.currentTarget.closest('.menu--item');
+          var isOpened = currentItem.classList.contains('menu--subitens__opened');
+          
           for (var j = 0; j < subnavs.length; j++) {
-
-            if(e.target.offsetParent != subnavs[j])
-              subnavs[j].classList.remove('menu--subitens__opened');          
-
+             subnavs[j].classList.remove('menu--subitens__opened');          
           }
-
-          e.target.offsetParent.classList.toggle('menu--subitens__opened');
-
+          
+          if (!isOpened) {
+              currentItem.classList.add('menu--subitens__opened');
+          }
       }, false);
-
     }
   }
-
-
 }, 100 );
-
